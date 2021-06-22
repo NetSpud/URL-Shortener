c00541bd719b692f499707e3cd399adf42c16034
NetSpud <edward@themorrisagency.co.uk>
2021-06-22 12:43:56 +0100

    Add heroku port control

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ app.get("/url/:slug", (req, res) => {
     });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
